b3005fa21a3a6b5ec87a75a9c5ee45757f495d6d
Thiago Cardoso <thiagojfcardoso@gmail.com>
2021-11-18 17:25:39 -0300

    fix: minor fixes

--- plugins/vuex-persist.js
+++ plugins/vuex-persist.js
@@ -1,12 +1,16 @@
 // eslint-disable-next-line import/no-named-as-default
+import * as Cookies from 'js-cookie'
+import cookie from 'cookie'
 import VuexPersistence from 'vuex-persist';
 
-export default ({ store }) => {
+export default ({ store, req, isDev }) => {
   window.onNuxtReady(() => {
     new VuexPersistence({
-      /* your options */
       key: 'brat_vuex',
-      modules: ['userSettings']
+      modules: ['userSettings'],
+      restoreState: (key, storage) =>process.client ? Cookies.getJSON(key) : cookie.parse(req.headers.cookie||'')[key],
+      saveState: (key, state, storage) =>
+      Cookies.set(key, value, { expires: 365, secure: !isDev }),
     }).plugin(store);
   });
 };
